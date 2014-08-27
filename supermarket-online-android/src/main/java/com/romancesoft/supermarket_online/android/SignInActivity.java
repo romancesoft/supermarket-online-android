93cac4f5171bb961675662a0e18f2848fb27899d
黄华宏 <romancesoft@gmail.com>
2014-08-27 17:04:26 +0800

    初始提交。

---
+++ supermarket-online-android/src/main/java/com/romancesoft/supermarket_online/android/SignInActivity.java
@@ -0,0 +1,15 @@
+package com.romancesoft.supermarket_online.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SignInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
